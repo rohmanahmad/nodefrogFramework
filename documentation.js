37378389605fbf00061f73bd842d4100520064a5
rohmanahmad <rohmanmail@gmail.com>
2019-11-04 23:15:23 +0700

    update documentation and authentication

---
+++ documentation.js
@@ -0,0 +1,177 @@
+'use strict'
+
+const { factory, models } = require('./config')
+const { version, name, description, license, contacts: {email, website} } = require('./package.json')
+const p = [
+    { name: 'find', method: 'POST', body: true },
+    { name: 'findOne', method: 'POST', body: true },
+    { name: 'create', method: 'POST', body: true },
+    { name: 'updateOne', method: 'POST', body: true },
+    { name: 'updateMany', method: 'POST', body: true },
+    { name: 'deleteOne', method: 'POST', body: true },
+    { name: 'deleteMany', method: 'POST', body: true },
+    { name: 'aggregate', method: 'POST', body: true } 
+]
+
+const servers = [
+    {
+        url: 'http://localhost:5050/'
+    }
+]
+
+const info = {
+    description,
+    version,
+    title: name,
+    contact: {
+        email,
+        website
+    },
+    license: {
+        name: license,
+        url: 'http://www.apache.org/licenses/LICENSE-2.0.html'
+    }
+}
+
+const schemes = [
+    'http',
+    'https'
+]
+
+const securityDefinitions = {
+    api_key: {
+        type: 'apiKey',
+        name: 'api_key',
+        in: 'header'
+    }
+}
+
+const tags = [
+]
+
+const response = {
+    // consumes: [
+    //     "application/x-www-form-urlencoded"
+    // ],
+    produces: [ "application/json" ],
+    responses:{
+        200: {
+            description: 'success',
+            schema: {
+                type: 'object'
+            }
+        }
+    }
+}
+
+const paths = function () {
+    const m = {...factory.models, ...models}
+    const objPath = {
+        '/authentication/login': {
+            'post': {
+                tags: ['Auth'],
+                summary: 'login',
+                parameters: [
+                    {
+                        name: 'username',
+                        in: 'formData',
+                        description: 'username',
+                        required: true
+                    },
+                    {
+                        name: 'password',
+                        in: 'formData',
+                        description: 'password',
+                        required: true
+                    }
+                ],
+                ...response
+            }
+        }
+    }
+    let index = 1
+    for (let modelName in m) {
+        const {api, paths} = m[modelName]
+        if (api) {
+            for (let pth of p) {
+                if (paths.indexOf(pth.name) > -1) {
+                    const cPath = {}
+                    const method = pth.method.toLocaleLowerCase()
+                    if (['get', 'post'].indexOf(method) === -1) {
+                        throw 'Invalid Method. Only GET or POST.'
+                    }
+                    let queries = []
+                    const modelObj = m[modelName]['schema']
+                    for (const name in modelObj) {
+                        const o = {
+                            in: 'query',
+                            name,
+                            description: `[String] value of ${name}`,
+                            required: false
+                        }
+                        if (modelObj[name] === '<string>') {
+                            o['schema'] = {
+                                type: 'string'
+                            }
+                        } else if (modelObj[name] === '<number>') {
+                            o['schema'] = {
+                                type: 'number'
+                            }
+                        } else if (modelObj[name] === '<boolean>') {
+                            o['schema'] = {
+                                type: 'boolean'
+                            }
+                        } else {
+                            o['schema'] = {
+                                type: 'string',
+                                description: `[${modelObj[name]}] accepted.`
+                            }
+                        }
+                        queries.push(o)
+                    }
+                    let requestBody = {
+                        in: 'body',
+                        description: 'Body',
+                        required: false
+                    }
+                    if (pth.name === 'updateMany') {
+                        queries = []
+                        requestBody['example'] = {
+                            criteria: 'object',
+                            update: 'object'
+                        }
+                    }
+                    queries.push(requestBody)
+                    let security = {}
+                    if (m[modelName]['auth']['status']) {
+                        if ((m[modelName]['auth']['routes']).indexOf(pth.name) > -1) security = { security: [{ 'api_key': [] }] }
+                    }
+                    cPath[method] = {
+                        tags: [`#${index} ${modelName}`],
+                        summary: modelName,
+                        parameters: queries,
+                        ...security,
+                        ...response
+                    }
+                    objPath[`/${modelName.toLowerCase()}/${pth.name}`] = cPath
+                }
+            }
+        }
+        index += 1
+    }
+    return {
+        swagger: '2.0',
+        info,
+        servers,
+        tags,
+        paths: objPath,
+        schemes,
+        securityDefinitions
+    }
+}
+const view = `<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><title>Documentation</title><link rel="stylesheet" type="text/css" href="./swagger-ui.css" ><link rel="icon" type="image/png" href="./favicon-32x32.png" sizes="32x32" /><link rel="icon" type="image/png" href="./favicon-16x16.png" sizes="16x16" /><style>html{box-sizing: border-box;overflow: -moz-scrollbars-vertical;overflow-y: scroll;}*,*:before,*:after{box-sizing: inherit;}body{margin:0;background: #fafafa;}</style></head><body><div id="swagger-ui"></div><script src="./swagger-ui-bundle.js"> </script><script src="./swagger-ui-standalone-preset.js"> </script><script>window.onload = function() { const ui = SwaggerUIBundle({ url: "swagger.json", dom_id: '#swagger-ui', deepLinking: true, presets: [ SwaggerUIBundle.presets.apis, SwaggerUIStandalonePreset ], plugins: [ SwaggerUIBundle.plugins.DownloadUrl ], layout: "StandaloneLayout" }); window.ui = ui; }</script></body></html>`
+
+module.exports = {
+    paths: paths(),
+    view
+}
